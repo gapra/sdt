2bec66313020f4fe0bfaf072707027a7ef1c7c4d
gapra <gapraart@gmail.com>
2014-11-06 16:45:31 +0700

    upload sample app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sdt::Application.config.secret_token = '2c74bf23016a3bbcc9d443fe4d689e1e6a9b7df3c96a5d88d5765220c23c83d0742814ae926d76229d0e4f27b48f705299977c3ae8f66a2a57215c4582a0d48b'
